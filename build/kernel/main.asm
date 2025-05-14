dc32a572d91a701038e1e27ec6eb1897c2d82170
u <gumanoid2082@gmail.com>
2025-05-14 16:09:24 -0400

    Update main.asm

--- build/kernel/main.asm
+++ build/kernel/main.asm
@@ -6,7 +6,6 @@ bits 16
 
 
 start:
-    ; clear screen
     mov ah, 0x06
     mov al, 0
     mov bh, 0x07
@@ -14,14 +13,12 @@ start:
     mov dx, 0x184F
     int 0x10
 
-    ; move cursor to 0,0
     mov ah, 0x02
     mov bh, 0x00
     mov dh, 0x00
     mov dl, 0x00
     int 0x10
 
-    ; print hello world message
     mov si, msg_hello
     call puts
 
@@ -29,24 +26,18 @@ start:
     cli
     hlt
 
-;
-; Prints a string to the screen
-; Params:
-;   - ds:si points to string
-;
 puts:
-    ; save registers we will modify
     push si
     push ax
     push bx
 
 .loop:
-    lodsb               ; loads next character in al
-    or al, al           ; verify if next character is null?
+    lodsb               
+    or al, al          
     jz .done
 
-    mov ah, 0x0E        ; call bios interrupt
-    mov bh, 0           ; set page number to 0
+    mov ah, 0x0E        
+    mov bh, 0           
     int 0x10
 
     jmp .loop
@@ -57,4 +48,4 @@ puts:
     pop si    
     ret
 
-msg_hello: db 'Hello world from KERNEL!', ENDL, 0
+msg_hello: db 'Hello world from kernel!', ENDL, 0
